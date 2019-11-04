65974f1de745cb78877fb6ce911b2ecbf380f9ba
dohyun <ehgus1551@naver.com>
2019-11-04 17:31:10 +0900

    GET|POST (url)

--- 05_Django/03_django_crud/articles/urls.py
+++ 05_Django/03_django_crud/articles/urls.py
@@ -10,15 +10,13 @@ app_name = 'articles'
 urlpatterns = [
 
     # create / update url 통합하기
-    path('<int:article_pk>/update/', views.update, name ='update'), 
     #GET (edit) / POST (update)
-
-
-
-    # path('<int:article_pk>/edit/', views.edit, name ='edit'), #  UPDATE Logic - 폼 전달
+     # GET(new) / POST (create)
+    path('<int:article_pk>/update/', views.update, name ='update'), 
     path('<int:article_pk>/delete/', views.delete, name ='delete'), #  DELETE Logic
     path('<int:article_pk>/', views.detail, name = 'detail'), # READ Logic - Detail
-    path('create/', views.create, name ='create'),  # GET(new) / POST (create)
-    # path('new/', views.new, name ='new'),    #CREATE Logic - 사용자에게 폼 전달
+    path('create/', views.create, name ='create'),  
     path('index/', views.index ,name ='index') #READ Logic - Index
+    # path('new/', views.new, name ='new'),    #CREATE Logic - 사용자에게 폼 전달
+    # path('<int:article_pk>/edit/', views.edit, name ='edit'), #  UPDATE Logic - 폼 전달
 ]
